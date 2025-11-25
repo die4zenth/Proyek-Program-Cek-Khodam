afca24e74650cacce37d6c525cb1beecbe3241ac
Rex Abhinaya <rexabhinaya@gmail.com>
2025-11-25 11:47:32 +0800

    Update cek_khodam.py

--- cek_khodam.py
+++ cek_khodam.py
@@ -4,7 +4,8 @@ def cek_khodam(nama):
     daftar_khodam = []
 
     khodam = random.choice(daftar_khodam)
-    return f"Khodam yang 'menemani' {nama}: {khodam}"
+    return f"Khodam yang menemani {nama}: {khodam}"
 
 nama = input("Masukkan nama kamu: ")
+umur = input("Masukkan umur kamu: ")
 print(cek_khodam(nama))
